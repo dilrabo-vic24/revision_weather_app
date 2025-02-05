c279ce3770e21f2854c71949ea9bdd93f306bb9c
dilrabo2023 <dilrabousubjonova0@gmail.com>
2025-02-05 10:43:38 +0500

    gif added

--- lib/logic/weather_notifier.dart
+++ lib/logic/weather_notifier.dart
@@ -24,18 +24,18 @@ class WeatherNotifier extends ChangeNotifier {
     String day = dateTime.substring(8, 10);
 
     Map<String, String> monthNames = {
-      "01": "January",
-      "02": "February",
-      "03": "March",
-      "04": "April",
+      "01": "Jan",
+      "02": "Feb",
+      "03": "Mar",
+      "04": "Apr",
       "05": "May",
-      "06": "June",
-      "07": "July",
-      "08": "August",
-      "09": "September",
-      "10": "October",
-      "11": "November",
-      "12": "December",
+      "06": "Jun",
+      "07": "Jul",
+      "08": "Aug",
+      "09": "Sep",
+      "10": "Oct",
+      "11": "Nov",
+      "12": "Dec",
     };
 
     String? monthName = monthNames[monthNumber];
